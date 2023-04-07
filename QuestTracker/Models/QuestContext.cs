fa71c7eb62d89763c3fdd4c926481aae5a3103d4
Andrei Ciucle <andrei.ciucle@outlook.com>
2023-04-07 16:04:34 +0300

    Add user creation function

---
+++ QuestTracker/Models/QuestContext.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace QuestTracker.Models
+{
+    public class QuestContext
+    {
+        private readonly IDbConnection _db;
+
+        public QuestContext(string? connectionString)
+        {
+            _db = new MySqlConnection(connectionString);
+        }
+
+        public async Task<IEnumerable<Quest>> GetAll()
+        {
+            return await _db.QueryAsync<Quest>("SELECT * FROM quests");
+        }
+
+        public async Task<Quest> GetById(int id)
+        {
+            return await _db.QueryFirstOrDefaultAsync<Quest>("SELECT * FROM quests WHERE id = @Id", new { Id = id });
+        }
+
+        public async Task<int> Create(Quest quest)
+        {
+            return await _db.ExecuteAsync("INSERT INTO quests (title, description, value) VALUE (@Title, @Description, @Value)", quest);
+        }
+    }
+}
